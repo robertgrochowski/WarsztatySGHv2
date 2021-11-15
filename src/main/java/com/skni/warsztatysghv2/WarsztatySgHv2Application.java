bd450b04dd7a5c16fa69a6e88c68a551675d07a9
Eryk Dobaj <dobajeryk@gmail.com>
2021-11-15 16:16:56 +0100

    Start point

--- src/main/java/com/skni/warsztatysghv2/WarsztatySgHv2Application.java
+++ src/main/java/com/skni/warsztatysghv2/WarsztatySgHv2Application.java
@@ -1,20 +1,20 @@
 package com.skni.warsztatysghv2;
 
-import com.skni.warsztatysghv2.registration.Student;
-import com.skni.warsztatysghv2.registration.StudentRequest;
-import com.skni.warsztatysghv2.registration.StudentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class WarsztatySgHv2Application {
 
     public static void main(String[] args) {
-        StudentService studentService = new StudentService();
-        StudentRequest studentRequest = new StudentRequest("Maciej", "Milewski");
-        Student student = studentService.create(studentRequest);
-        System.out.println(student);
-//        SpringApplication.run(WarsztatySgHv2Application.class, args);
+        SpringApplication.run(WarsztatySgHv2Application.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void doAfterStartup() {
+        // tutaj wywolaj kod
     }
 
 }
